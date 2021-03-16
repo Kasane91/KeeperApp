6ec3e601eb0410d22d7b477517cda5c3c14595ee
Sondre Søråsdekkan <sondre.sorasdekkan@gmail.com>
2021-03-16 12:43:28 +0100

    Destructured and rendered notes dynamically

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+const createNoteEntry = (entry) => {
+  return (
+    <Note key={entry.id} title={entry.title} content={entry.content}></Note>
+  );
+};
 
 let App = () => {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNoteEntry)}
       <Footer />
     </div>
   );
