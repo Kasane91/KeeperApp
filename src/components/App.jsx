bfb13066790797147e4dc0411145bfac420ed5d3
Sondre Søråsdekkan <sondre.sorasdekkan@gmail.com>
2021-03-19 16:21:25 +0100

    Added an add note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,37 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from "react";
+
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
 
-const createNoteEntry = (entry) => {
-  return (
-    <Note key={entry.id} title={entry.title} content={entry.content}></Note>
-  );
-};
+import CreateArea from "./CreateArea";
 
 let App = () => {
+  const [noteArray, setNoteArray] = useState([]);
+
+  const updateNoteArray = (note) => {
+    setNoteArray((prevItems) => {
+      return [...prevItems, note];
+    });
+  };
+
+  const handleDelete = (id) => {
+    console.log("I got deleted" + id);
+  };
+
   return (
     <div>
       <Header />
-      {notes.map(createNoteEntry)}
+      <CreateArea onAdd={updateNoteArray} />
+      {noteArray.map((entry, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={entry.title}
+          content={entry.content}
+          onDelete={handleDelete}
+        ></Note>
+      ))}
       <Footer />
     </div>
   );
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({ title: "", content: "" });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setNote((prevValue) => {
+      return { ...prevValue, [name]: value };
+    });
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+          value={note.title}
+        />
+        <textarea
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={note.content}
+        />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            props.onAdd(note);
+            setNote({ title: "", content: "" });
+          }}
+        >
+          Add
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
